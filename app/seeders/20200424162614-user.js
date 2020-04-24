c469492abe2e1f394202f6bb8cd2467e32e1fb7c
maximelarrieu <maxime.larrieu@ynov.com>
2020-04-24 20:04:38 +0200

    :zap: first seeders

---
+++ app/seeders/20200424162614-user.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      name: 'Maxime',
+      email: 'maxime.larrieu@ynov.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
