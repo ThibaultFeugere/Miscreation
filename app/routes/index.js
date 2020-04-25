b6f11b4a95355f24e16f11fd6bdab510971c3f13
maximelarrieu <maxime.larrieu@ynov.com>
2020-04-25 18:12:35 +0200

    :zap: route to specific id

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,36 @@
 var express = require('express');
 var router = express.Router();
-const users = require('../models/user')
+const models = require('../models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Miscreate',
-    users: users.findAll()
-  });
+router.get('/', function (req, res, next) {
+  models.Distortion.findAll()
+    .then(function (distortions) {
+      res.render('index', {
+        title: 'Miscreate',
+        distortions: distortions
+      });
+    });
+});
+
+router.get('/distortion/:id', function (req, res, next) {
+  models.Distortion.findByPk(req.params.id)
+    .then(function (Distortion) {
+      res.render('show', {
+        distortion: Distortion
+      });
+    });
 });
 
 /* Route /status */
-router.get("/status", function(req, res) {
+router.get("/status", function (req, res) {
   res.send("OK !");
 });
 
 /* Route /index */
 router.get("/test", function (req, res) {
   res.render('index', {
-      title: 'PLOP NOOB'
+    title: 'PLOP NOOB'
   })
 })
 
