5f08c5038d7e28d7ef2756f3ffeb995d02d9434e
Thibault <thibaultfeugere@gmail.com>
2020-04-28 09:39:43 +0200

    Cleaning file and adding showAll for API

--- app/routes/distortion.js
+++ app/routes/distortion.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models')
 
+/* GUI PART */
+
 router.get('/show/:id', function (req, res, next) {
   models.Distortion.findByPk(req.params.id)
     .then(function (Distortion) {
@@ -11,8 +13,19 @@ router.get('/show/:id', function (req, res, next) {
     });
 });
 
+router.get('/create', function (req, res, next) {
+    res.render('distortion/create');
+});
+
+/* API PART */
+
 router.get('/api/show/:id', function (req, res, next) {
   models.Distortion.findByPk(req.params.id)
+    .then( (result) => res.json(result))
+});
+
+router.get('/api/showall/', function (req, res, next) {
+  models.Distortion.findAll()
     .then( (result) => res.json(result))
 });
 
@@ -23,10 +36,6 @@ router.delete('/api/:id', function (req, res, next) {
     }
   })
   .then((result) => res.json(result))
-});
-
-router.get('/create', function (req, res, next) {
-    res.render('distortion/create');
 });
 
 module.exports = router;
