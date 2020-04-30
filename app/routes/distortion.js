fe510514cf7e30facb69c8343b7092a6e9c29cef
Thibault <thibaultfeugere@gmail.com>
2020-04-30 11:14:34 +0200

    Adding new routes, renaming and adding two buttons

--- app/routes/distortion.js
+++ app/routes/distortion.js
@@ -14,7 +14,7 @@ router.get('/distortion/show/:id', function (req, res, next) {
     });
 });
 
-router.get('/create', function (req, res, next) {
+router.get('/distortion/create', function (req, res, next) {
   Distortion.insertOne(title, description, image, release_date, image, finder)
   // .then(function (Distortion) {
   res.render('distortion/create', {
@@ -28,6 +28,16 @@ router.get('/create', function (req, res, next) {
   // });
 });
 
+router.get('/distortion/delete/:id', function (req, res) {
+  Distortion.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function (distortions) {
+      res.redirect('/');
+    });
+});
 /* API PART */
 
 router.get('/api/distortion/show/', function (req, res) {
@@ -43,8 +53,6 @@ router.get('/api/distortion/show/:id', function (req, res) {
 });
 
 router.post('/api/distortion/create', bodyParser, function (req, res) {
-  console.log(req.headers);
-  console.log(req.body);
   Distortion.create({
       title: req.body.title,
       description: req.body.description,
@@ -56,7 +64,24 @@ router.post('/api/distortion/create', bodyParser, function (req, res) {
     .catch(err => res.json(err));
 })
 
-router.delete('/api/distortion/:id', function (req, res) {
+router.put('/api/distortion/update/:id', bodyParser, function (req, res) {
+  Distortion.update({
+      title: req.body.title,
+      description: req.body.description,
+      release_date: req.body.release_date,
+      image: req.body.image,
+      finder: req.body.finder
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((result) => res.json(result))
+    .catch(err => res.json(err));
+})
+
+router.delete('/api/distortion/delete/:id', function (req, res) {
   Distortion.destroy({
       where: {
         id: req.params.id
