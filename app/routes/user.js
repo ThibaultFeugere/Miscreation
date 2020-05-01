ba364acde2b6bb437696ad3cc8aea633f20ba47c
maximelarrieu <maxime.larrieu@ynov.com>
2020-05-01 17:05:08 +0200

    :sparkles: api users routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,19 +2,52 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models').User
 
-/* GET users listing. */
+/* API PART */
 
-router.get('/user/show/id', function(req, res, next) {
+router.get('api/user/show/', function (req, res) {
   User.findAll()
-    .then(function (Users) {
-      res.render('user/index', {
-        users: Users
-      });
-    });
+    .then((result) => res.json(result))
+    .catch(err => res.json(err));
 });
 
-router.get('/user/show/', function(req, res, next) {
-  
+router.get('api/user/show/:id', function (req, res) {
+  User.findByPk(req.params.id)
+    .then((result) => res.json(result))
+    .catch(err => res.json(err))
+})
+
+router.post('/api/user/create', function (req, res) {
+  User.create({
+      name: req.params.name,
+      email: req.params.email,
+      password: req.params.password
+    })
+    .then((result) => res.json(result))
+    .catch(err => res.json(err));
+})
+
+router.put('/api/user/update/:id', function (req, res) {
+  User.update({
+      name: req.params.name,
+      email: req.params.email,
+      password: req.params.password
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((result) => res.json(result))
+    .catch(err => res.json(err));
+})
+
+router.delete('/api/user/delete/:id', function (req, res) {
+  User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((result) => res.json(result))
+    .catch(err => res.json(err));
 })
 
 module.exports = router;
