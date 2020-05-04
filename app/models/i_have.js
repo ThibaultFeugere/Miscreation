0d2fd6c5ae9638d559f56eb37db7072a475a1625
maximelarrieu <maxime.larrieu@ynov.com>
2020-05-04 14:30:37 +0200

    :zap: belongsTo

--- app/models/i_have.js
+++ app/models/i_have.js
@@ -6,8 +6,18 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   i_Have.associate = function (models) {
     // associations can be defined here
-    i_Have.belongsToMany(models.User);
-    i_Have.belongsToMany(models.Disortion);
+    // i_Have.belongsToMany(models.User, {
+    //   through: models.User
+    // });
+    // i_Have.belongsToMany(models.Disortion, {
+    //   through: models.User
+    // });
+    models.User.belongsToMany(models.Distortion, {
+      through: i_Have
+    })
+    models.Distortion.belongsToMany(models.User, {
+      through: i_Have
+    })
   };
   return i_Have;
 };
